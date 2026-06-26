bf13340953028669ecac62e049b83d8f5f2e0ab2
Martik-k <kosiv.marta@gmail.com>
2026-06-26 13:45:32 +0300

    feat: initialize arm workspace with urdf kinematics and rviz config

---
+++ src/arm_bringup/launch/arm_standalone.launch.py
@@ -0,0 +1,32 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    arm_description_dir = get_package_share_directory('arm_description')
+    xacro_file = os.path.join(arm_description_dir, 'urdf', 'arm_standalone.urdf.xacro')
+
+    robot_description_content = Command(['xacro ', xacro_file])
+
+    return LaunchDescription([
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description_content}]
+        ),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui'
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', os.path.join(arm_description_dir, 'rviz', 'arm.rviz')]
+        )
+    ])
